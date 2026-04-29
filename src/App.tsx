0654cfb3976e2c452a546c6435a102bbf872d608
twoChar <tushar20254@iiitd.ac.in>
2026-04-30 00:38:51 +0530

    Fix router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <FilterProvider>
           <Routes>
             <Route path="/" element={<RevenueProfiling />} />
